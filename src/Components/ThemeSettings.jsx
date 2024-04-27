af49ca26472e4c0e5ca86bf476e9631fdc24ef32
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-04-27 12:35:20 +0530

    compleated Navbar and sidebar

---
+++ src/Components/ThemeSettings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ThemeSettings = () => {
+  return (
+    <div>
+      Themesettings
+    </div>
+  );
+}
+
+export default ThemeSettings;
---
+++ src/Pages/Charts/Financial.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Financial = () => {
+  return (
+    <div>
+      Financial
+
+    </div>
+  );
+}
+
+export default Financial;
